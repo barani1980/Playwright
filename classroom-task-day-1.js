bc9a07faa12c778afb0d8bf610556da0fd5ba9df
Baranidharan <41632491+barani1980@users.noreply.github.com>
2025-08-16 13:18:24 +0530

    Add files via upload

---
+++ classroom-task-day-1.js
@@ -0,0 +1,16 @@
+let firstName = "John"
+let companyName = "Microsoft"
+let mobileNumber = 9176371551
+let isAutomation = true
+let hasPlaywright
+
+console.log(`First Name: ${firstName} Type: ${typeof firstName}`)
+console.log(`Company Name: ${companyName} Type: ${typeof companyName}`)
+console.log(`Mobile Number: ${mobileNumber} Type: ${typeof mobileNumber}`)
+console.log(`Is Automation: ${isAutomation} Type: ${typeof isAutomation}`)
+console.log(`Has Playwright: ${hasPlaywright} Type: ${typeof hasPlaywright}`)
+
+var name = "John"
+let name = "Rajinikanth"
+var name = "Shivaji"
+console.log(name)
